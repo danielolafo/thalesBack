4405694b31b8658d8db0835cea39e6486ed8d6ab
dolopezASW <55155910+dolopezASW@users.noreply.github.com>
2021-11-21 20:19:44 -0500

    Pruebas unitarias

--- src/test/java/com/daniel/lopez/prueba/tecnica/PruebaTecnicaApplicationTests.java
+++ src/test/java/com/daniel/lopez/prueba/tecnica/PruebaTecnicaApplicationTests.java
@@ -1,13 +1,37 @@
 package com.daniel.lopez.prueba.tecnica;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.daniel.lopez.prueba.tecnica.service.EmployeeService;
 
 @SpringBootTest
 class PruebaTecnicaApplicationTests {
+	
+	@Autowired
+	private EmployeeService employeeService;
 
+	
 	@Test
-	void contextLoads() {
+	void testGetAll() {
+		assertTrue(employeeService.getAll().size()>0);
 	}
+	
+	/*@Test
+	void testById() {
+		assertTrue(employeeService.getById(1L)!=null);
+	}*/
+	
+	/*@Test
+	void testByIdWrong() {
+		Assertions.assertThrows(Exception.class, () -> {
+			employeeService.getById(-12L);
+		});
+		//assertTrue(employeeService.getById(-1L)!=null);
+	}*/
 
 }
